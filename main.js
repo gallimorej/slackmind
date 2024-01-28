b4df887bb48e478857a8be67d825a0444830c14d
Jeff Gallimore <jeff.gallimore@excella.com>
2024-01-28 17:23:03 -0500

    Initial commit for hello world app

---
+++ main.js
@@ -0,0 +1,18 @@
+// main.js
+
+const { app, BrowserWindow } = require('electron')
+const { WebClient } = require('@slack/web-api');
+
+function createWindow () {
+  let win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+    }
+  })
+
+  win.loadFile('index.html')
+}
+
+app.whenReady().then(createWindow)
